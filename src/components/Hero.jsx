5fb827b037ec23193d7085ca230f316cb5b79fcf
Israel <ogungbeisrael966@gmail.com>
2026-02-23 03:35:33 +0100

    ggg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { copy } from "../data/Copy";
+import { copy } from "../data/copy";
 import { images } from "../data/images";
 
 const Hero = () => {
@@ -6,17 +6,15 @@ const Hero = () => {
     <section className="section bg-black text-white">
       <div className="container-custom grid md:grid-cols-2 gap-10 items-center">
         <div className="space-y-6">
-          <h1 className="heading-primary">
-            {copy.hero.headline}
-          </h1>
-          <p className="text-body">
-            {copy.hero.subheadline}
-          </p>
-         <a href="http://chat.whatsapp.com/ETOJe01JuWFG3Q6H7llW0N" target="_blank" rel="noopener noreferrer">
-          <button className="btn-primary">
-            Join Now
-          </button>
-        </a>
+          <h1 className="heading-primary">{copy.hero.headline}</h1>
+          <p className="text-body">{copy.hero.subheadline}</p>
+          <a
+            href="http://chat.whatsapp.com/ETOJe01JuWFG3Q6H7llW0N"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="btn-primary">Join Now</button>
+          </a>
         </div>
 
         <img
